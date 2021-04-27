da9e217dea0bc92be31a794eba06f13982aaea4f
Choi Jiwon <winning1993@naver.com>
2021-04-28 08:12:16 +0900

    [ADD] 테이블 저장 겨우 성공..

---
+++ Telephone Diary/Telephone Diary/dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Mobiles].[Table]
+(
+	[Id] INT NOT NULL , 
+    [First] VARCHAR(50) NULL, 
+    [Last] VARCHAR(50) NULL, 
+    [Mobile] VARCHAR(50) NOT NULL, 
+    [Email] VARBINARY(50) NULL, 
+    [Category] VARBINARY(50) NULL, 
+    PRIMARY KEY ([Mobile])
+)
